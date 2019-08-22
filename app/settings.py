6dee34eb1100979a95477ae451f9e921f32c381f
atoivanen <toivanen.aurora@gmail.com>
2019-08-22 15:24:26 +0300

    Set debug to true

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gyzbftcqe34+kfd-yzqr@10t0qbp!rd)x1h!h3^d=3f+h0k-%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
